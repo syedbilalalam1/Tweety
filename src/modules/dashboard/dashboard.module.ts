b5519e2e1c9237ffd47fdb4663c4c9de930866b2
Bilal Alam <wagofficial01@gmail.com>
2025-02-25 17:25:06 +0500

    Delete src directory

--- src/modules/dashboard/dashboard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DashboardController } from './dashboard.controller';
-import { ManageTweetModule } from '../manage-tweet/manage-tweet.module';
-
-@Module({
-  imports: [ManageTweetModule],
-  controllers: [DashboardController],
-})
-export class DashboardModule {} 
